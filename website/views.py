dd8b476fda0b18414440c0ffa86cb0e951af69c7
fahhrad <ward.coessens@gmail.com>
2015-10-08 15:49:24 +0200

    added background images

--- website/views.py
+++ website/views.py
@@ -3,21 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 
 # Index page
+from website.models import TheaterPlay
+
+
 def index(request):
     return render(request, "index.html")
 
 
 def projects(request):
-    return render(request, "projects.html")
+    theater_plays = TheaterPlay.objects.all()
+    return render(request, "projects.html", {'theater_plays': theater_plays,
+                                             'body_class': 'projects'})
 
 
 def press(request):
-    return render(request, "press.html")
+    return render(request, "press.html", {'body_class': 'press'})
 
 
 def images(request):
-    return render(request, "images.html")
+    return render(request, "images.html", {'body_class': 'images'})
 
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "contact.html", {'body_class': 'contact'})
+
